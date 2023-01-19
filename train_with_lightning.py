16877db90d0a947462ed9a6b6c0dbb8bf9992e99
Emanuele Rucci <rucci.emanuele.work@gmail.com>
2023-01-19 19:44:51 +0100

    Implementati DGCNN e ResNet per classification con lightning

--- train_with_lightning.py
+++ train_with_lightning.py
@@ -8,10 +8,150 @@ from lightly.loss.ntx_ent_loss import NTXentLoss
 from torch.utils.data import DataLoader
 from datasets.data import ShapeNetRender, ModelNet40SVM
 from util import IOStream, AverageMeter
+import torch.nn as nn
+import torch.nn.functional as F
 
-from models.dgcnn import DGCNN, ResNet, DGCNN_partseg
+#from models.dgcnn import DGCNN, ResNet, DGCNN_partseg
 from torchvision.models import resnet50, resnet18
 
+
+
+def knn(x, k):
+    inner = -2*torch.matmul(x.transpose(2, 1), x)
+    xx = torch.sum(x**2, dim=1, keepdim=True)
+    pairwise_distance = -xx - inner - xx.transpose(2, 1)
+ 
+    idx = pairwise_distance.topk(k=k, dim=-1)[1]   # (batch_size, num_points, k)
+    return idx
+
+
+def get_graph_feature(x, k=20, idx=None):
+    batch_size = x.size(0)
+    num_points = x.size(2)
+    x = x.view(batch_size, -1, num_points)
+    if idx is None:
+        idx = knn(x, k=k)   # (batch_size, num_points, k)
+
+    
+    #Mio commento----
+    #device = torch.device('cuda:1')
+    #----
+    if (torch.cuda.is_available()):
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+    #------
+
+    idx_base = torch.arange(0, batch_size, device=device).view(-1, 1, 1)*num_points
+
+    idx = idx + idx_base
+
+    idx = idx.view(-1)
+ 
+    _, num_dims, _ = x.size()
+
+    x = x.transpose(2, 1).contiguous()   # (batch_size, num_points, num_dims)  -> (batch_size*num_points, num_dims) #   batch_size * num_points * k + range(0, batch_size*num_points)
+    feature = x.view(batch_size*num_points, -1)[idx, :]
+    feature = feature.view(batch_size, num_points, k, num_dims) 
+    x = x.view(batch_size, num_points, 1, num_dims).repeat(1, 1, k, 1)
+    
+    feature = torch.cat((feature-x, x), dim=3).permute(0, 3, 1, 2).contiguous()
+  
+    return feature
+
+
+class DGCNN(pl.LightningModule):
+    def __init__(self,args,cls = -1) -> None:
+        super(DGCNN,self).__init__()
+        self.args = args
+        self.k = args.k
+
+        self.bn1 = nn.BatchNorm2d(64)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.bn4 = nn.BatchNorm2d(256)
+        self.bn5 = nn.BatchNorm1d(args.emb_dims)
+
+        self.conv1 = nn.Sequential(nn.Conv2d(6, 64, kernel_size=1, bias=False),self.bn1, nn.LeakyReLU(negative_slope=0.2))
+        self.conv2 = nn.Sequential(nn.Conv2d(64*2, 64, kernel_size=1, bias=False),self.bn2,nn.LeakyReLU(negative_slope=0.2))
+        self.conv3 = nn.Sequential(nn.Conv2d(64*2, 128, kernel_size=1, bias=False),self.bn3,nn.LeakyReLU(negative_slope=0.2))
+        self.conv4 = nn.Sequential(nn.Conv2d(128*2, 256, kernel_size=1, bias=False), self.bn4, nn.LeakyReLU(negative_slope=0.2))
+        self.conv5 = nn.Sequential(nn.Conv1d(512, args.emb_dims, kernel_size=1, bias=False), self.bn5, nn.LeakyReLU(negative_slope=0.2))
+    
+    
+        if cls != -1:
+            self.linear1 = nn.Linear(self.args.emb_dims*2, 512, bias=False)
+            self.bn6 = nn.BatchNorm1d(512)
+            self.dp1 = nn.Dropout(p=self.args.dropout)
+            self.linear2 = nn.Linear(512, 256)
+            self.bn7 = nn.BatchNorm1d(256)
+            self.dp2 = nn.Dropout(p=self.args.dropout)
+            self.linear3 = nn.Linear(256, output_channels)
+        
+        self.cls = cls
+        
+        self.inv_head = nn.Sequential(
+                            nn.Linear(self.args.emb_dims * 2, args.emb_dims),
+                            nn.BatchNorm1d(self.args.emb_dims),
+                            nn.ReLU(inplace=True),
+                            nn.Linear(self.args.emb_dims, 256)
+                            )
+
+    def forward(self, x):
+        batch_size = x.size(0)
+        x = get_graph_feature(x, k=self.k)
+        x = self.conv1(x)
+        x1 = x.max(dim=-1, keepdim=False)[0]
+
+        x = get_graph_feature(x1, k=self.k)
+        x = self.conv2(x)
+        x2 = x.max(dim=-1, keepdim=False)[0]
+
+        x = get_graph_feature(x2, k=self.k)
+        x = self.conv3(x)
+        x3 = x.max(dim=-1, keepdim=False)[0]
+
+        x = get_graph_feature(x3, k=self.k)
+        x = self.conv4(x)
+        x4 = x.max(dim=-1, keepdim=False)[0]
+
+        x = torch.cat((x1, x2, x3, x4), dim=1)
+
+        x = self.conv5(x)
+        x1 = F.adaptive_max_pool1d(x, 1).view(batch_size, -1)
+        x2 = F.adaptive_avg_pool1d(x, 1).view(batch_size, -1)
+        x = torch.cat((x1, x2), 1)
+        
+        feat = x
+        if self.cls != -1:
+            x = F.leaky_relu(self.bn6(self.linear1(x)), negative_slope=0.2)
+            x = self.dp1(x)
+            x = F.leaky_relu(self.bn7(self.linear2(x)), negative_slope=0.2)
+            x = self.dp2(x)
+            x = self.linear3(x)
+        
+        inv_feat = self.inv_head(feat)
+        
+        return x, inv_feat, feat 
+    
+class ResNet(pl.LightningModule):
+    def __init__(self, model, feat_dim = 2048):
+        super(ResNet, self).__init__()
+        self.resnet = model
+        self.resnet.fc = nn.Identity()
+        
+        self.inv_head = nn.Sequential(
+                            nn.Linear(feat_dim, 512, bias = False),
+                            nn.BatchNorm1d(512),
+                            nn.ReLU(inplace=True),
+                            nn.Linear(512, 256, bias = False)
+                            ) 
+        
+    def forward(self, x):
+        x = self.resnet(x)
+        x = self.inv_head(x)
+        
+        return x
 
 
 class CrosspointLightning (pl.LightningModule):
@@ -24,13 +164,13 @@ class CrosspointLightning (pl.LightningModule):
         # models
         # ------------
         if args.model == 'dgcnn':
-            self.point_model = DGCNN(args) #todo in lightning
+            self.point_model = DGCNN(args) 
         elif args.model == 'dgcnn_seg':
             self.point_model = DGCNN_partseg(args) #todo in lightning
         else:
             raise Exception("Not implemented")
 
-        self.img_model = ResNet(resnet50(), feat_dim = 2048) #todo in lightning
+        self.img_model = ResNet(resnet50(), feat_dim = 2048) 
         self.criterion = NTXentLoss(temperature = 0.1)
 
         if args.enable_wandb == True:
@@ -85,8 +225,6 @@ class CrosspointLightning (pl.LightningModule):
         return [optimizer], [lr_scheduler]
 
 
-
-
 def train(args,io):
 
     if args.enable_wandb == True:
@@ -116,8 +254,6 @@ def train(args,io):
     trainer = pl.Trainer.from_argparse_args(args,max_epochs=-1)
     trainer.fit(model, train_loader)
 
-
-    
 
 def cli_main():
     # ------------
@@ -167,8 +303,6 @@ def cli_main():
     # ------------ 
     if not args.eval:
         train(args, io)
-
-
 
 
 if __name__ == '__main__':
